7483bba021528fda8f119672a74724c7ea062758
quantumburrito <107977173+quantumburrito@users.noreply.github.com>
2024-09-06 20:39:25 +0000

    created example utility function. need to test

--- handlers/message_handler.go
+++ handlers/message_handler.go
@@ -1,3 +1,5 @@
+// Package handlers provides functions for handling Discord message events.
+// This file contains the MessageHandler function, which handles incoming message events and responds accordingly.
 package handlers
 
 import (
@@ -5,6 +7,13 @@ import (
 	"github.com/disgoorg/disgo/events"
 )
 
+// MessageHandler is a function that handles incoming message events and responds accordingly.
+// It checks the content of the message and performs different actions based on the command.
+// If the message content is "!ping", it sends a "Pong!" response.
+// If the message content is "!hello", it sends a "Hello there!" response.
+// For any other message content, it sends a "Whoops, Unknown command" response.
+// The function uses the event.Client().Rest().CreateMessage method to send the responses.
+// If there is an error sending the message, it logs the error.
 func MessageHandler(event *event.MessageCreate) {
 	if event.Message.Author.Bot {
 		return
@@ -12,7 +21,7 @@ func MessageHandler(event *event.MessageCreate) {
 
 	switch event.Message.Content {
 	case "!ping":
-		_, err :- event.Client().Rest().CreateMessage(event.ChannelID, "Pong!")
+		_, err := event.Client().Rest().CreateMessage(event.ChannelID, "Pong!")
 		if err != nil {
 			log.Printf("Error sending message: %v", err)
 		}
