136ca173637d8cdb8b3df970298284046fc18fd7
quantumburrito <107977173+quantumburrito@users.noreply.github.com>
2024-09-09 20:28:52 +0000

    corrected ddl

--- SQL/starboard-ddl.sql
+++ SQL/starboard-ddl.sql
@@ -1,10 +1,10 @@
-CEATE TABLE starboard (
+CREATE TABLE starboard (
     id SERIAL PRIMARY KEY,
     message_id TEXT NOT NULL,
-    channel_id TEXT NOT NULL.
+    channel_id TEXT NOT NULL,
     author_id TEXT NOT NULL,
     content TEXT NOT NULL,
     star_count INT NOT NULL DEFAULT 1,
     posted_at TIMESTAMP DEFAULT NOW(),
     UNIQUE (message_id)
-)
+);
