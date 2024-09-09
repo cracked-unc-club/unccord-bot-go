2ae299941ab427fde2b78c1115d6efef37e85cfe
quantumburrito <107977173+quantumburrito@users.noreply.github.com>
2024-09-09 19:14:31 +0000

    Created Handlers:OnReactionAdd, GetStarredMessage,InsertStarredMessage,

--- uncord-bot-go/handlers/message_handler.go
+++ uncord-bot-go/handlers/message_handler.go
@@ -26,6 +26,56 @@ func GetStarredMessage(messageID string) (int, error) {
 	return starCount, err
 }
 
+//OnReactionAdd handles star reactions and posts the message to the starboard if it reacehs the threshold
+func OnReactionAdd(event *events.GuildMessageReactionAdd) {
+	// Check if the reaction is a star emoji
+	if event.Emoji.Name != "⭐" {
+		//Fetch the message that was reacted to 
+		message, err := event.Client().Rest().GetMessage(event.ChannelID, event.MessageID)
+		if err != nil {
+			log.Printf("Error fetching message: %v", err)
+			return
+		}
+
+		// Insert or update the star count in the database
+		err  = InsertStarredMessage(event.MessageID.String(), event.ChannelID.String(), message.Author.ID.String(), message.Content)
+		if err != nil {
+			log.Printf("Error inserting stared message: %v", err)
+			return
+		}
+
+		// check if the message has reached the star threshold
+		starCount, err := GetStarredMessage(event.MessageID.String())
+		if err != nil {
+			log.Printf("Error fetching star count: %v", err)
+			return
+		}
+		
+		if starCount >= config.Starboard.StarThreshold {
+			// Call the PostToStarboard function here
+			PostToStarboard(event, message, starCount)
+		}
+	}
+}
+
+func PostToStarboard(event *events.GuildMessageReactionAdd, message *discord.Message, starCount int) {
+	embed := discord.NewEmbedBuilder().
+		SetTitle("Starred Message").
+		SetDescription(message.Content).
+		AddField("Stars", string(starCount), true).
+		SetAuthorName(message.Author.Username).
+		SetTimestamp(message.CreatedAt).
+		SetFooterText("From #" + event.ChannelID.String()).
+		Build()
+	
+	_, err := event.Client().Rest().CreateMessage(config.Starboard.StarboardChannelID, discord.NewMessageCreateBuilder().AddEmbeds(embed).Build())
+	if err != nil {
+		log.Printf("Error sending message to starboard: %v", err)
+		
+	}
+
+}
+
 // OnMessageCreate handles message creation events.
 func OnMessageCreate(event *events.MessageCreate) {
 	if event.Message.Author.Bot {
