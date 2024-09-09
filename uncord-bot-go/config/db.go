1e4e498e0f7dd1f1b9e83c785d88f58a9797d6bc
quantumburrito <107977173+quantumburrito@users.noreply.github.com>
2024-09-09 18:23:08 +0000

    created db config setup function

---
+++ uncord-bot-go/config/db.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+
+
+var DB *sql.DB
+
+func ConnectDB() {
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	var err error
+	DB, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Cannot ping the database: %v, err")
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("Cannot ping the database: %v, err")
+	}
+	log.Println("Connected to the database")
+}
